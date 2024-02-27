658ddee87ed5d5ba605a03ecf844b16e4f956ebc
aramrw <106574385+aramrw@users.noreply.github.com>
2024-02-26 18:41:04 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Next.js 14 + Tuari 2.0.0 Beta Template (No Mobile).
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="mt-20 animate-pulse text-center font-bold">Next.js 14.1.0 + Tauri 2.0.0 Beta!</h1>
+      <h2 className="text-center font-medium">Aint nothin here, get to hackin!</h2>
+    </main>
+  );
+}
