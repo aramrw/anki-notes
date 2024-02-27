3969fa93d06056843683af32558997be74cf126a
aramrw <106574385+aramrw@users.noreply.github.com>
2024-02-26 18:41:04 -0800

    Initialize next-ui

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="animate-pulse pt-20 text-center font-bold">Next.js v14.1.0 + Tauri v2.0.0 Beta + NextUIv2.29!</h1>
+      <h2 className="text-center font-medium">Aint nothin here, get to hackin!</h2>
+    </main>
+  );
+}
